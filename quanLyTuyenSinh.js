ea018f0169d4c79795cc3b67c18f0654277346e0
XuanTrung2909 <trung.nx290989@gmail.com>
2020-12-30 17:09:58 +0700

    upload

---
+++ quanLyTuyenSinh.js
@@ -0,0 +1,77 @@
+function  tinhDiem(event){
+
+    event.preventDefault();
+
+    // Nhập dữ liệu 
+
+    var mon1 = document.getElementById("mon1").value;
+    var mon2 = document.getElementById("mon2").value;
+    var mon3 = document.getElementById("mon3").value;
+    var khuVuc = document.getElementById("khu__vuc").value;
+    var doiTuong = document.getElementById("doi__tuong").value;
+
+    // Xử lý dữ liệu
+
+    mon1 = parseFloat(mon1);
+    mon2 = parseFloat(mon2);
+    mon3 = parseFloat(mon3);
+    doiTuong = parseInt(doiTuong);
+
+    var checkKhuVuc = xuLyKhuVucUuTien(khuVuc);
+    checkKhuVuc = parseFloat(checkKhuVuc);
+    var checkDoiTuong = xuLyDoiTuongUuTien(doiTuong);
+    checkDoiTuong = parseFloat(checkDoiTuong);
+
+    var tongDiem = tinhTongDiem(mon1, mon2, mon3, checkKhuVuc, checkDoiTuong);
+
+    // In Dữ liêu
+
+    document.getElementById("footer__qlts").innerHTML = "Tổng điểm là: " + tongDiem;
+
+    
+    
+}
+
+
+function xuLyKhuVucUuTien(a){
+    var diemKhuVuc = 0;
+    switch(a){
+        case "A":
+            diemKhuVuc = 2;
+            break;
+        case "B":
+            diemKhuVuc = 1;
+            break;
+        case "C":
+            diemKhuVuc = 0.5;
+            break;
+        case "X":
+            diemKhuVuc = 0;
+            break;
+    }
+    return diemKhuVuc;
+}
+function xuLyDoiTuongUuTien(b){
+    var diemDoiTuong = 0;
+    switch(b){
+        case 1:
+            diemDoiTuong = 2.5;
+            break;
+        case 2:
+            diemDoiTuong = 1.5;
+            break;
+        case 3:
+            diemDoiTuong = 1;
+            break;
+        case 0:
+            diemDoiTuong = 0;
+            break;
+    }
+    return diemDoiTuong;
+}
+
+
+function tinhTongDiem(a, b, c, d, e){
+    var tong = (a + b + c + d + e);  
+    return tong;
+}
